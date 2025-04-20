8f7e857b8675f52174706d66cb94240dff235d96
Thanakorn Nitsiri <tnitsiri@gmail.com>
2025-04-20 10:22:29 +0700

    style: [main] ellipse shape

---
+++ src/app/[lng]/layout-style/(components)/ellipse.tsx
@@ -0,0 +1,18 @@
+import styles from './styles.module.scss';
+
+/**
+ * ANCHOR Ellipse
+ * @date 20/04/2025 - 10:16:09
+ *
+ * @returns {*}
+ */
+const Ellipse = () => {
+  // ANCHOR Render
+  return (
+    <div className={styles.shape}>
+      <div className={styles.ellipse} />
+    </div>
+  );
+};
+
+export default Ellipse;
--- src/app/[lng]/layout-style/page.tsx
+++ src/app/[lng]/layout-style/page.tsx
@@ -2,6 +2,7 @@ import Header from '@/components/header';
 import Triangle from './(components)/triangle';
 import Square from './(components)/square';
 import Circle from './(components)/circle';
+import Ellipse from './(components)/ellipse';
 import styles from '@/assets/styles/page.module.scss';
 import { getT } from '@/app/i18n';
 import { Link } from '@/components/link';
@@ -51,6 +52,9 @@ const Page = async () => {
               </Flex>
               <Flex>
                 <Circle />
+              </Flex>
+              <Flex>
+                <Ellipse />
               </Flex>
             </Flex>
           </Flex>
